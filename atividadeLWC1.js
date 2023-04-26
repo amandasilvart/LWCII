2bed2472453dc065be993760847a7b9bcf31b1c3
Amanda Silva <106178378+amandasilvart@users.noreply.github.com>
2023-04-26 17:48:21 -0300

    Add files via upload

---
+++ atividadeLWC1.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class AtividadeLWC1 extends LightningElement {
+
+    contato = [
+        {cod:1, primeironome:"Amanda" ,  sobrenome:"sobrenome", idade:"xx", email:"email@exemplo.com", 
+      clasificacao:"Hot", empresa:"Não tem", receitaanual:"20000"}
+        
+    ]
+        
+}
